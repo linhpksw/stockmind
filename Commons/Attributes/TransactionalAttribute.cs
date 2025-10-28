ab4cc620e0b740dc766813efd84278443faddd86
vietsonvtpt <vietsonvtpt@gmail.com>
2025-10-28 21:54:27 +0700

    Update TransactionalAttribute.cs

--- Commons/Attributes/TransactionalAttribute.cs
+++ Commons/Attributes/TransactionalAttribute.cs
@@ -2,21 +2,28 @@
 using AspectCore.DynamicProxy;
 using stockmind.Models;
 
-namespace stockmind.Commons.Attributes {
+namespace stockmind.Commons.Attributes
+{
     [AttributeUsage(AttributeTargets.Method)]
-    public class TransactionalAttribute : AbstractInterceptorAttribute {
-        public override async Task Invoke(AspectContext context, AspectDelegate next) {
+    public class TransactionalAttribute : AbstractInterceptorAttribute
+    {
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
             var dbContext = context.ServiceProvider.GetService<StockMindDbContext>();
-            if (dbContext == null) {
+            if (dbContext == null)
+            {
                 await next(context);
                 return;
             }
 
             await using var transaction = await dbContext.Database.BeginTransactionAsync();
-            try {
+            try
+            {
                 await next(context);
                 await transaction.CommitAsync();
-            } catch {
+            }
+            catch
+            {
                 await transaction.RollbackAsync();
                 throw;
             }
